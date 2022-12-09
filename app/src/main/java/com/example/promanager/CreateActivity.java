a47a4f6fd8a4290d88c05af4d0d0beb5b7af4661
Nguyen Thinh <nhoxthinhcute2001@gmail.com>
2022-12-09 17:27:54 +0700

    BE for new Layout createActivity

--- app/src/main/java/com/example/promanager/CreateActivity.java
+++ app/src/main/java/com/example/promanager/CreateActivity.java
@@ -3,12 +3,39 @@ package com.example.promanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.w3c.dom.Text;
 
 public class CreateActivity extends AppCompatActivity {
+    TextInputEditText activity_name, activity_manager, activity_deadline, activity_describe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create);
+        activity_name = (TextInputEditText) findViewById(R.id.activity_name_tiet);
+        activity_manager = (TextInputEditText) findViewById(R.id.activity_manager_tiet);
+        activity_deadline = (TextInputEditText) findViewById(R.id.activity_deadline_tiet);
+        activity_describe = (TextInputEditText) findViewById(R.id.activity_describe_tiet);
+
+        TextView confirm_btn = (TextView) findViewById(R.id.confirm_button);
+        confirm_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addInformation();
+            }
+        });
+    }
+
+    private void addInformation(){
+        activity_name.getText();
+        activity_manager.getText();
+        activity_deadline.getText();
+        activity_describe.getText();
     }
 }
